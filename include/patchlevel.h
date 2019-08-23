99fc7ad2db3d1a078265408c2e69f6b9fcf8d44a
Janne Gustafsson <janne.gustafsson@hyvanmielenpelit.fi>
2019-08-23 22:33:05 +0300

    Patch level changed to 4.0.12 following 4.0.11 release.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 11
+#define PATCHLEVEL 12
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
