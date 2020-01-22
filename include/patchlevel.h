476fe1945b936da770da3fa56ab633333bed6902
Janne Gustafsson <janne.gustafsson@hyvanmielenpelit.fi>
2020-01-22 23:01:25 +0200

    Patch level changed to 4.0.20 following the 4.0.19 Beta Test release. All changes from the beta test go here.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -9,7 +9,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 19
+#define PATCHLEVEL 20
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
