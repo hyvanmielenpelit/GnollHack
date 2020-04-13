7faa0b7957042b4c476ef2d551514934ae6c4517
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2020-04-13 17:00:39 +0300

    Made graden rooms even a bit rarer.

--- src/mklev.c
+++ src/mklev.c
@@ -883,7 +883,7 @@ makelevel()
 		if (!res && u_depth > 20 && !rn2(5))
 			res = mkroom(DRAGONLAIR);
 
-		if (u_depth >= 2 && u_depth < depth(&medusa_level) && !rn2(5))
+		if (u_depth >= 2 && u_depth < depth(&medusa_level) && !rn2(6))
 			(void)mkroom(GARDEN);
 
 	}
