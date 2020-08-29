e398722f0f865c4eb4adb323514b885aed2dff70
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2020-08-30 00:48:28 +0300

    Changed animation speed.

--- src/animation.c
+++ src/animation.c
@@ -213,7 +213,7 @@ NEARDATA struct animation_definition animations[NUM_ANIMATIONS + 1] =
     },
     { "human-knight-female-cast-animation", HUMAN_KNIGHT_FEMALE_CAST_ANIMATION_TILES,
         HUMAN_KNIGHT_FEMALE_CAST_ANIMATION_FRAMES, HUMAN_KNIGHT_FEMALE_CAST_ANIMATION_OFF,
-        1,
+        2,
         ANIMATION_PLAY_TYPE_PLAYED_SEPARATELY, ANIMATION_MAIN_TILE_USE_FIRST,
         AUTODRAW_NONE,
         { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
