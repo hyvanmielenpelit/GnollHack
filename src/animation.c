e52a31a0a3d79bdafac5c15574685bc0f0e7a899
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-01-27 01:39:26 +0200

    Some fixes.

--- src/animation.c
+++ src/animation.c
@@ -589,11 +589,11 @@ NEARDATA struct animation_definition animations[MAX_ANIMATIONS] =
     { "human-healer-female-potion-drink-animation", ANIMATION_TYPE_NORMAL, HUMAN_HEALER_FEMALE_POTION_DRINK_ANIMATION_TILES,
       HUMAN_HEALER_FEMALE_POTION_DRINK_ANIMATION_FRAMES, HUMAN_HEALER_FEMALE_POTION_DRINK_ANIMATION_OFF,
       1,
-      2,
+      3,
       ANIMATION_PLAY_TYPE_PLAYED_SEPARATELY, ANIMATION_MAIN_TILE_USE_LAST,
       AUTODRAW_NONE,
       { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
-      14, 17,
+      13, 15,
       NO_ENLARGEMENT,
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     },
@@ -604,7 +604,7 @@ NEARDATA struct animation_definition animations[MAX_ANIMATIONS] =
       ANIMATION_PLAY_TYPE_PLAYED_SEPARATELY, ANIMATION_MAIN_TILE_USE_LAST,
       AUTODRAW_NONE,
       { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
-      8, 18,
+      15, 18,
       HUMAN_HEALER_FEMALE_CAST_DIR_ENLARGEMENT,
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     },
@@ -615,7 +615,7 @@ NEARDATA struct animation_definition animations[MAX_ANIMATIONS] =
       ANIMATION_PLAY_TYPE_PLAYED_SEPARATELY, ANIMATION_MAIN_TILE_USE_LAST,
       AUTODRAW_NONE,
       { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
-      9, 18,
+      15, 18,
       HUMAN_HEALER_FEMALE_CAST_INDIR_ENLARGEMENT,
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     },
