1ef4f6bb6570b293bd133cf571e065ef3a4bca55
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-01-29 11:49:37 +0200

    Small fix to inventory mark with party members.

--- win/win32/mhmap.c
+++ win/win32/mhmap.c
@@ -4102,7 +4102,7 @@ paintTile(PNHMapWindow data, int i, int j, RECT * rect)
                                         display_this_status_mark = TRUE;
                                     break;
                                 case STATUS_MARK_INVENTORY:
-                                    if (!loc_is_you && ispet && count_unworn_items(mtmp->minvent) > 0)
+                                    if (!loc_is_you && ispet && !mtmp->ispartymember && count_unworn_items(mtmp->minvent) > 0)
                                         display_this_status_mark = TRUE;
                                     break;
                                 default:
