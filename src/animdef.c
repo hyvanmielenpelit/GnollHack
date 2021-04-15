b294d1f88a658fecc3029c416189ce9fda831b4f
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-04-15 11:38:56 +0300

    Added statue trap animation.

--- src/animdef.c
+++ src/animdef.c
@@ -167,6 +167,17 @@ NEARDATA struct animation_definition animations[MAX_ANIMATIONS] =
       NO_ENLARGEMENT,
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     },
+    { "statue-trap-stand-animation", ANIMATION_TYPE_NORMAL, STATUE_TRAP_STAND_ANIMATION_TILES,
+      STATUE_TRAP_STAND_ANIMATION_FRAMES,
+      1,
+      2,
+      ANIMATION_PLAY_TYPE_ALWAYS, ANIMATION_MAIN_TILE_IGNORE,
+      AUTODRAW_NONE,
+      { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
+      -1, -1,
+      NO_ENLARGEMENT,
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    },
     { "look-cursor-animation", ANIMATION_TYPE_NORMAL, LOOK_CURSOR_ANIMATION_TILES,
       LOOK_CURSOR_ANIMATION_FRAMES,
       1,
