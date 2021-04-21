f8393700f22b3609ab634005aab750929ff2aabc
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-04-21 18:46:31 +0300

    Small fix to special levels with corrmazes.

--- src/sp_lev.c
+++ src/sp_lev.c
@@ -7828,7 +7828,7 @@ sp_lev *lvl;
      * is currently not possible, we overload the corrmaze flag for this
      * purpose.
      */
-    if (!level.flags.corrmaze)
+    //if (!level.flags.corrmaze)
         wallification(1, 0, COLNO - 1, ROWNO - 1);
 
     count_features();
