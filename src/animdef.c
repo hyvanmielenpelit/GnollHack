d64c468285fd9744cca9d31bf3f0d88d6f1f6df5
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-05-01 14:35:55 +0300

    Small fix to animation speed.

--- src/animdef.c
+++ src/animdef.c
@@ -3220,7 +3220,7 @@ NEARDATA struct animation_definition animations[MAX_ANIMATIONS] =
     { "vibrating-square-animation", ANIMATION_TYPE_NORMAL, VIBRATING_SQUARE_ANIMATION_TILES,
         VIBRATING_SQUARE_ANIMATION_FRAMES,
         1,
-        2,
+        3,
         ANIMATION_PLAY_TYPE_ALWAYS, ANIMATION_MAIN_TILE_USE_FIRST,
         AUTODRAW_NONE,
         { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
