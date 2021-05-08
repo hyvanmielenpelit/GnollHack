2b3643b4dfb7f486bdb808e8b070a0715abb111f
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-05-09 00:36:19 +0300

    More tests.

--- win/win32/winclisrv/GnollHackClient/GnollHackClient/MainPage.xaml.cs
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient/MainPage.xaml.cs
@@ -26,6 +26,22 @@ namespace GnollHackClient
 {
     public partial class MainPage : ContentPage
     {
+        class Person
+        {
+            public Person(string name, DateTime birthday, Color favoriteColor)
+            {
+                this.Name = name;
+                this.Birthday = birthday;
+                this.FavoriteColor = favoriteColor;
+            }
+
+            public string Name { private set; get; }
+
+            public DateTime Birthday { private set; get; }
+
+            public Color FavoriteColor { private set; get; }
+        };
+
         private HubConnection connection;
         private string _message = "";
         private string _message2 = "";
@@ -61,6 +77,60 @@ namespace GnollHackClient
 
             //firstButton.ImageSource = ImageSource.FromResource("GnollHackClient.Assets.button_normal.png", assembly);
             myImage.Source = ImageSource.FromResource("GnollHackClient.Assets.button_normal.png", assembly);
+
+
+            // Define some data.
+            List<Person> people = new List<Person>
+            {
+                new Person("Abigail", new DateTime(1975, 1, 15), Color.Aqua),
+                new Person("Bob", new DateTime(1976, 2, 20), Color.Black),
+                // ...etc.,...
+                new Person("Yvonne", new DateTime(1987, 1, 10), Color.Purple),
+                new Person("Zachary", new DateTime(1988, 2, 5), Color.Red)
+            };
+
+            // Create the ListView.
+            // Source of data items.
+            myListView.ItemsSource = people;
+            myListView.ItemTemplate = new DataTemplate(() =>
+                {
+                    // Create views with bindings for displaying each property.
+                    Label nameLabel = new Label();
+                    nameLabel.SetBinding(Label.TextProperty, "Name");
+
+                    Label birthdayLabel = new Label();
+                    birthdayLabel.SetBinding(Label.TextProperty,
+                        new Binding("Birthday", BindingMode.OneWay,
+                            null, null, "Born {0:d}"));
+
+                    BoxView boxView = new BoxView();
+                    boxView.SetBinding(BoxView.ColorProperty, "FavoriteColor");
+
+                    // Return an assembled ViewCell.
+                    return new ViewCell
+                    {
+                        View = new StackLayout
+                        {
+                            Padding = new Thickness(0, 5),
+                            Orientation = StackOrientation.Horizontal,
+                            Children =
+                                {
+                                    boxView,
+                                    new StackLayout
+                                    {
+                                        VerticalOptions = LayoutOptions.Center,
+                                        Spacing = 0,
+                                        Children =
+                                        {
+                                            nameLabel,
+                                            birthdayLabel
+                                        }
+                                        }
+                                }
+                        }
+                    };
+                });
+
 
             Device.StartTimer(TimeSpan.FromSeconds(1f / 40), () =>
             {
