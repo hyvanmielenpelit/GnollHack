80a1935026405c1b2d0452651cf7a9582b1809a4
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-05-16 16:36:02 +0300

    Some final changes.

--- win/curses/curswins.c
+++ win/curses/curswins.c
@@ -581,13 +581,10 @@ write_char(WINDOW * win, int x, int y, nethack_char nch)
     int printedchar;
     curses_toggle_color_attr(win, nch.color, nch.attr, ON);
 
-#if 0
-    if (flags.ibm2utf8 && nch.ch >= 0 && nch.ch < 256)
+    if (flags.ibm2utf8 && nch.ch >= 0 && nch.ch < 256) /* Note requires that ncursesw has been installed */
         printedchar = (int)cp437toUnicode[nch.ch];
     else
-#endif
-
-    printedchar = nch.ch;
+        printedchar = nch.ch;
 
 #ifdef PDCURSES
     mvwaddrawch(win, y, x, printedchar);
