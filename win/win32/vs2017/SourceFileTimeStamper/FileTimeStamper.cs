b7fda4b012c5a7b9ed78f60c872b5f1d3551384f
Tommi Gustafsson <tommi.gustafsson@hyvanmielenpelit.fi>
2021-09-13 18:22:16 +0300

    Updated to use /* */ comment notation.

--- win/win32/vs2017/SourceFileTimeStamper/FileTimeStamper.cs
+++ win/win32/vs2017/SourceFileTimeStamper/FileTimeStamper.cs
@@ -8,8 +8,8 @@ namespace SourceFileTimeStamper
 {
     public class FileTimeStamper
     {
-        string _modifiedDateString = "//** GnollHack File Changed: ";
-        string _stampDateString = "//** GnollHack File Time Stamped: ";
+        string _modifiedDateString = "/* GnollHack File Has Last Changed: {0} */";
+        string _stampDateString = "/* GnollHack File Time Stamped: {0} */";
         string _separatorString = "";
 
         public FileTimeStamper()
@@ -21,13 +21,16 @@ namespace SourceFileTimeStamper
         {
             var lines = File.ReadAllLines(file.FullName);
 
+            string modifiedDateStringStart = _modifiedDateString.Substring(0, _modifiedDateString.Length - 6);
+            string stampDateStringStart = _stampDateString.Substring(0, _stampDateString.Length - 6);
+
             var fileLastWriteTimeString = file.LastWriteTimeUtc.ToString("u");
             var nowString = DateTime.Now.ToUniversalTime().ToString("u");
 
-            var modifiedTimeStamp = _modifiedDateString + fileLastWriteTimeString;
-            var stampTimeStamp = _stampDateString + nowString;
+            var modifiedTimeStamp = string.Format(_modifiedDateString, fileLastWriteTimeString);
+            var stampTimeStamp = string.Format(_stampDateString, nowString);
 
-            if (lines.Length < 3 || !lines[0].StartsWith(_modifiedDateString) || !lines[1].StartsWith(_stampDateString) || lines[2] != _separatorString)
+            if (lines.Length < 3 || !lines[0].StartsWith(modifiedDateStringStart) || !lines[1].StartsWith(stampDateStringStart) || lines[2] != _separatorString)
             {
                 //No stamp, insert
                 List<string> lines2 = new List<string>(lines);
@@ -39,11 +42,11 @@ namespace SourceFileTimeStamper
 
                 File.WriteAllLines(file.FullName, lines2);
             }
-            else if (lines[0].StartsWith(_modifiedDateString) && lines[1].StartsWith(_stampDateString) && lines[2] == _separatorString)
+            else if (lines[0].StartsWith(modifiedDateStringStart) && lines[1].StartsWith(stampDateStringStart) && lines[2] == _separatorString)
             {
                 //Already stamped
-                var stampedDateEnd = lines[1].Substring(_stampDateString.Length);
-                var stampedDate = DateTime.ParseExact(stampedDateEnd, "u", CultureInfo.InvariantCulture);
+                var stampedDateString = lines[1].Substring(stampDateStringStart.Length, lines[1].Length - stampDateStringStart.Length - 3);
+                var stampedDate = DateTime.ParseExact(stampedDateString, "u", CultureInfo.InvariantCulture);
                 if(file.LastWriteTimeUtc.AddSeconds(-10) < stampedDate && stampedDate < file.LastWriteTimeUtc.AddSeconds(10))
                 {
                     //Already stamped, do nothing
