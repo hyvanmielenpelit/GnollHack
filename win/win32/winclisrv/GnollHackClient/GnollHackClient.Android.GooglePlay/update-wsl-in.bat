62e47d8ebcacea79e2d2d7aa5752f7844f783291
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-12-21 23:13:43 +0200

    Small capitalization fix.

--- win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
@@ -3,6 +3,6 @@ if not exist c:\wsl-in\assetpack mkdir c:\wsl-in\assetpack
 if not exist c:\wsl-in\assetpack\Assets mkdir c:\wsl-in\assetpack\Assets
 if not exist c:\wsl-in\assetpack\aapt2output mkdir c:\wsl-in\assetpack\aapt2output
 copy ..\bank\Mobile\*.* c:\wsl-in\assetpack\Assets
-copy ..\..\..\ext\Android\aapt2 c:\wsl-in\assetpack
+copy ..\..\..\ext\android\aapt2 c:\wsl-in\assetpack
 copy ..\winclisrv\GnollHackClient\GnollHackClient.Android.GooglePlay\AndroidManifest.xml c:\wsl-in\assetpack
 copy ..\winclisrv\GnollHackClient\GnollHackClient.Android.GooglePlay\createassetpack.sh c:\wsl-in\assetpack
