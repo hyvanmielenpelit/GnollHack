4634067780cdc328495ca0d3f96a3787e4c5fd12
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-12-22 15:53:28 +0200

    Fixes to Android asset pack generation.

--- win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
@@ -2,6 +2,7 @@ if not exist c:\wsl-in mkdir c:\wsl-in
 if not exist c:\wsl-in\assetpack mkdir c:\wsl-in\assetpack
 if not exist c:\wsl-in\assetpack\Assets mkdir c:\wsl-in\assetpack\Assets
 if not exist c:\wsl-in\assetpack\aapt2output mkdir c:\wsl-in\assetpack\aapt2output
+del c:\wsl-in\assetpack\Assets\*.*
 copy ..\bank\Mobile\*.* c:\wsl-in\assetpack\Assets
 copy ..\..\..\ext\android\aapt2 c:\wsl-in\assetpack
 copy ..\winclisrv\GnollHackClient\GnollHackClient.Android.GooglePlay\AndroidManifest.xml c:\wsl-in\assetpack
