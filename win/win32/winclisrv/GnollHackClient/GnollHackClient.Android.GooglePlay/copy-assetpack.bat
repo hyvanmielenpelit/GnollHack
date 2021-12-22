9e481cf8a4da1896fb7b71444565b3689802a5ae
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2021-12-22 17:39:57 +0200

    Added mkdir assetpacks to gnollhackdroid post build event. Created a new Windows batch file.

---
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/copy-assetpack.bat
@@ -0,0 +1,2 @@
+if not exist ..\winclisrv\GnollHackClient\GnollHackClient.Android\assetpacks mkdir ..\winclisrv\GnollHackClient\GnollHackClient.Android\assetpacks
+copy c:\wsl-in\assetpack\assets.zip ..\winclisrv\GnollHackClient\GnollHackClient.Android\assetpacks
