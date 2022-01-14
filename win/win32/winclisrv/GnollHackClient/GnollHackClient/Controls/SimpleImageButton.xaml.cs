8a2f87475ac6daa8ccd49ca331059fc9f379800b
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2022-01-14 22:09:49 +0200

    Some fixes.

--- win/win32/winclisrv/GnollHackClient/GnollHackClient/Controls/SimpleImageButton.xaml.cs
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient/Controls/SimpleImageButton.xaml.cs
@@ -15,8 +15,6 @@ namespace GnollHackClient.Controls
         public static readonly BindableProperty ImgSourcePathProperty = BindableProperty.Create(nameof(ImgSourcePath), typeof(string), typeof(SimpleImageButton), string.Empty);
         public static readonly BindableProperty GridWidthProperty = BindableProperty.Create(nameof(GridWidth), typeof(double), typeof(SimpleImageButton), 45.0);
         public static readonly BindableProperty GridHeightProperty = BindableProperty.Create(nameof(GridHeight), typeof(double), typeof(SimpleImageButton), 45.0);
-        public static readonly BindableProperty ImgWidthProperty = BindableProperty.Create(nameof(ImgWidth), typeof(double), typeof(SimpleImageButton), 45.0);
-        public static readonly BindableProperty ImgHeightProperty = BindableProperty.Create(nameof(ImgHeight), typeof(double), typeof(SimpleImageButton), 45.0);
 
         public SimpleImageButton()
         {
@@ -31,17 +29,6 @@ namespace GnollHackClient.Controls
             get => (string)GetValue(SimpleImageButton.ImgSourcePathProperty);
             set => SetValue(SimpleImageButton.ImgSourcePathProperty, value);
         }
-        public double ImgWidth
-        {
-            get => (double)GetValue(SimpleImageButton.ImgWidthProperty);
-            set => SetValue(SimpleImageButton.ImgWidthProperty, value);
-        }
-        public double ImgHeight
-        {
-            get => (double)GetValue(SimpleImageButton.ImgHeightProperty);
-            set => SetValue(SimpleImageButton.ImgHeightProperty, value);
-        }
-
         public double GridWidth
         {
             get => (double)GetValue(SimpleImageButton.GridWidthProperty);
@@ -64,14 +51,8 @@ namespace GnollHackClient.Controls
             else
                 imgsidewidth = Math.Min(75.0, Math.Max(35.0, canvaswidth / Math.Max(1, PortraitButtonsInRow)));
 
-            double imgsideheight = imgsidewidth;
-            double gridsidewidth = imgsidewidth;
-            double gridsideheight = imgsideheight;
-
-            GridWidth = gridsidewidth;
-            GridHeight = gridsideheight;
-            ImgWidth = imgsidewidth;
-            ImgHeight = imgsideheight;
+            GridWidth = imgsidewidth;
+            GridHeight = imgsidewidth;
         }
 
         private void ViewButton_Clicked(object sender, EventArgs e)
