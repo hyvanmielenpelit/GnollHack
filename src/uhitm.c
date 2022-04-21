1fdd438658e5d584395309d221236d420cc6aafd
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2022-04-21 14:18:53 +0300

    Fixed a bug with sfx sounds.

--- src/uhitm.c
+++ src/uhitm.c
@@ -251,12 +251,12 @@ struct monst *mtmp;
     if (Role_if(PM_KNIGHT) && u.ualign.type == A_LAWFUL
         && (!mon_can_move(mtmp)
             || (is_fleeing(mtmp) && !mtmp->mavenge))) {
-        play_sfx_sound(GHSOUND_CAITIFF);
+        play_sfx_sound(SFX_CAITIFF);
         You_ex(ATR_NONE, CLR_MSG_ATTENTION, "caitiff!");
         adjalign(-1);
     } else if (Role_if(PM_SAMURAI) && is_peaceful(mtmp)) {
         /* attacking peaceful creatures is bad for the samurai's giri */
-        play_sfx_sound(GHSOUND_CAITIFF);
+        play_sfx_sound(SFX_CAITIFF);
         You_ex(ATR_NONE, CLR_MSG_ATTENTION, "dishonorably attack the innocent!");
         adjalign(-1);
     }
