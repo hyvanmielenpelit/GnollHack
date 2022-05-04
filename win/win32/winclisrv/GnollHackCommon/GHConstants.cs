be724009a47ae93f08897f050b2ccc67d0a96cc1
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2022-05-04 17:21:38 +0300

    Small fix to flags.

--- win/win32/winclisrv/GnollHackCommon/GHConstants.cs
+++ win/win32/winclisrv/GnollHackCommon/GHConstants.cs
@@ -149,21 +149,21 @@ namespace GnollHackCommon
     public enum ChmodPermissions : uint
     {
         // user permissions
-        S_IRUSR = 0x100,
-        S_IWUSR = 0x80,
-        S_IXUSR = 0x40,
+        S_IRUSR = 0x100U,
+        S_IWUSR = 0x80U,
+        S_IXUSR = 0x40U,
 
         // group permission
-        S_IRGRP = 0x20,
-        S_IWGRP = 0x10,
-        S_IXGRP = 0x8,
+        S_IRGRP = 0x20U,
+        S_IWGRP = 0x10U,
+        S_IXGRP = 0x8U,
 
         // other permissions
-        S_IROTH = 0x4,
-        S_IWOTH = 0x2,
-        S_IXOTH = 0x1,
+        S_IROTH = 0x4U,
+        S_IWOTH = 0x2U,
+        S_IXOTH = 0x1U,
 
-        S_IALL = 0x1FF,
+        S_IALL = 0x1FFU,
     }
 
     [Flags]
