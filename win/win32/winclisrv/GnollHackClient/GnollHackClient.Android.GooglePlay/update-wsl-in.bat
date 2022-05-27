768e7dd9233dd13881b26b41c926dc6b6117f0f6
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2022-05-27 09:45:27 +0300

    iOS banks will now be updated.

--- win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
+++ win/win32/winclisrv/GnollHackClient/GnollHackClient.Android.GooglePlay/update-wsl-in.bat
@@ -6,10 +6,14 @@ if not exist c:\wsl-in\assetpack\Assets\banks mkdir c:\wsl-in\assetpack\Assets\b
 del /Q c:\wsl-in\assetpack\Assets\banks\*.*
 if not exist c:\wsl-in\assetpack\aapt2output mkdir c:\wsl-in\assetpack\aapt2output
 if not exist ..\winclisrv\GnollHackClient\GnollHackClient.Android\banks mkdir ..\winclisrv\GnollHackClient\GnollHackClient.Android\banks
+if not exist ..\winclisrv\GnollHackClient\GnollHackClient.iOS\banks mkdir ..\winclisrv\GnollHackClient\GnollHackClient.iOS\banks
 echo Copying banks to wsl-in's banks
 copy ..\bank\Mobile\*.* c:\wsl-in\assetpack\Assets\banks
 del /Q ..\winclisrv\GnollHackClient\GnollHackClient.Android\banks\*.*
-echo Copying banks to APK's banks
+del /Q ..\winclisrv\GnollHackClient\GnollHackClient.iOS\banks\*.*
+echo Copying banks to Android banks
 copy ..\bank\Mobile\*.* ..\winclisrv\GnollHackClient\GnollHackClient.Android\banks
+echo Copying banks to iOS banks
+copy ..\bank\Mobile\*.* ..\winclisrv\GnollHackClient\GnollHackClient.iOS\banks
 echo Copying aapt2 to wsl-in\assetpack
 copy ..\..\..\ext\android\linux-x64\aapt2 c:\wsl-in\assetpack
