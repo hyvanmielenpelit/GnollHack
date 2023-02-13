429c1c4c1b7831e2af116dfe2ac8902b1a7500f8
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-02-13 22:37:11 +0200

    Fix to curses.

--- win/curses/cursstat.c
+++ win/curses/cursstat.c
@@ -674,7 +674,7 @@ boolean border;
      */
 
     number_of_lines = (iflags.wc2_statuslines < 3) ? 2 : (iflags.wc2_statuslines > 7) ? 8 : iflags.wc2_statuslines;
-    fieldorder = flags.altstatuslineorder ? ((number_of_lines >= 8) ? eightlineorder_alt : (number_of_lines == 7) ? sevenlineorder_alt : (number_of_lines == 6) ? sixlineorder_alt :
+    fieldorder = flags.fullstatuslineorder ? ((number_of_lines >= 8) ? eightlineorder_alt : (number_of_lines == 7) ? sevenlineorder_alt : (number_of_lines == 6) ? sixlineorder_alt :
         (number_of_lines == 5) ? fivelineorder_alt : (number_of_lines == 4) ? fourlineorder_alt :
         (number_of_lines != 3) ? twolineorder : threelineorder_alt) : ((number_of_lines >= 8) ? eightlineorder : (number_of_lines == 7) ? sevenlineorder : (number_of_lines == 6) ? sixlineorder :
         (number_of_lines == 5) ? fivelineorder : (number_of_lines == 4) ? fourlineorder : 
