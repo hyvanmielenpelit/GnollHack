c97bc2b15e2104166bf130cdfa38ba1d0c9631ec
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-02-23 23:05:04 +0200

    Added undead soundsets to vampire.

--- src/sounds.c
+++ src/sounds.c
@@ -1247,7 +1247,7 @@ boolean dopopup, fromchatmenu;
         boolean isnight = night();
         boolean kindred = (Upolyd && (u.umonnum == PM_VAMPIRE || u.umonnum == PM_VAMPIRE_MAGE
                                       || u.umonnum == PM_VAMPIRE_LORD || u.umonnum == PM_VAMPIRE_KING));
-        boolean nightchild =
+        boolean nightchild = !iflags.using_gui_sounds && 
             (Upolyd && (u.umonnum == PM_WOLF || u.umonnum == PM_WINTER_WOLF
                         || u.umonnum == PM_WINTER_WOLF_CUB));
         const char *racenoun =
