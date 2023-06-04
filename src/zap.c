b9e7413e16314e96c4e612f9a9f0b223a75d8c8b
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-06-04 18:49:18 +0300

    Some more further fixes. Wish tracker may work too.

--- src/zap.c
+++ src/zap.c
@@ -11429,6 +11429,10 @@ retry:
                             ? "Oops!  %s away from you!"
                             : "Oops!  %s to the floor!");
 
+#ifdef WISH_TRACKER
+        /* write it out to our universal wishtracker file */
+        trackwish(bufcpy);
+#endif
         /* The(aobjnam()) is safe since otmp is unidentified -dlc */
         (void) hold_another_object(otmp, oops_msg,
                                    The(aobjnam(otmp, verb)),
