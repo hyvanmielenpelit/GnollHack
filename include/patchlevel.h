7e9fdded57dbe31b3c050ba8e82a353ba6c0cff8
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-06-28 11:17:01 +0300

    Updated version to 4.1.2.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -11,7 +11,7 @@
 /*
  * PATCHLEVEL is updated for each release.
  */
-#define PATCHLEVEL 1
+#define PATCHLEVEL 2
 /*
  * Incrementing EDITLEVEL can be used to force invalidation of old bones
  * and save files.
@@ -19,7 +19,7 @@
  * EDITLEVEL is also used to indicate the pre-alpha / alpha / beta version if PRE_RELEASE compiler preprocessor option is on.
  * Change EDITLEVEL when the changes to the game are save-file-breaking.
  */
-#define EDITLEVEL 5
+#define EDITLEVEL 0
 
 /*
  * HOTFIXLEVEL indicates the hot fix number. 
@@ -44,7 +44,7 @@
  * PP = patch level, ee = edit level, L = literal suffix "L",
  * with all four numbers specified as two hexadecimal digits.
  */
-#define VERSION_COMPATIBILITY       0x04010104UL
+#define VERSION_COMPATIBILITY       0x04010200UL
 #define NEW_VERSION_INFO_VERSION    0x04010104UL
 
 
