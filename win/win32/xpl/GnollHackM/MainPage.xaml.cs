13b91f12295754fbacb3429b68225271fb533001
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-08-12 16:56:59 +0300

    Added SkiaSharp and Json libraries to GnollHackM.

--- win/win32/xpl/GnollHackM/MainPage.xaml.cs
+++ win/win32/xpl/GnollHackM/MainPage.xaml.cs
@@ -1,4 +1,6 @@
-﻿namespace GnollHackM;
+﻿using SkiaSharp;
+
+namespace GnollHackM;
 
 public partial class MainPage : ContentPage
 {
@@ -7,6 +9,7 @@ public partial class MainPage : ContentPage
 	public MainPage()
 	{
 		InitializeComponent();
+        canvasView.InvalidateSurface();
 	}
 
 	private void OnCounterClicked(object sender, EventArgs e)
@@ -20,5 +23,25 @@ public partial class MainPage : ContentPage
 
 		SemanticScreenReader.Announce(CounterBtn.Text);
 	}
+
+    private void canvasView_PaintSurface(object sender, SkiaSharp.Views.Maui.SKPaintSurfaceEventArgs e)
+    {
+        SKImageInfo info = e.Info;
+        SKSurface surface = e.Surface;
+        SKCanvas canvas = surface.Canvas;
+        string str = "";
+        float xText = 0;
+        float yText = 100;
+        float canvaswidth = canvasView.CanvasSize.Width;
+        float canvasheight = canvasView.CanvasSize.Height;
+
+        using (SKPaint textPaint = new SKPaint())
+        {
+            str = "Test! (" + canvaswidth + ", " + canvasheight + ")";
+            textPaint.TextSize = 26;
+            textPaint.Color = SKColors.Red;
+            canvas.DrawText(str, xText, yText, textPaint);
+        }      
+    }
 }
 
--- win/win32/xpl/GnollHackM/MauiProgram.cs
+++ win/win32/xpl/GnollHackM/MauiProgram.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.Extensions.Logging;
+using SkiaSharp.Views.Maui.Controls.Hosting;
 
 namespace GnollHackM;
 
@@ -9,7 +10,8 @@ public static class MauiProgram
 		var builder = MauiApp.CreateBuilder();
 		builder
 			.UseMauiApp<App>()
-			.ConfigureFonts(fonts =>
+            .UseSkiaSharp()
+            .ConfigureFonts(fonts =>
 			{
 				fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
 				fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
