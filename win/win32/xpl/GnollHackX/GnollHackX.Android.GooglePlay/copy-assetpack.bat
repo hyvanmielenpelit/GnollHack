7eb2b4e1c54399bb846621380700a2618cd44bfd
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-08-12 15:46:02 +0300

    Adjusted some spell levels. Added libraries back to GnollHackM. Added assetpack, bank and gnh file copying to GnollHackM.

--- win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/copy-assetpack.bat
+++ win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/copy-assetpack.bat
@@ -3,3 +3,7 @@ echo Start executing copy-assetpack.bat
 if not exist ..\xpl\GnollHackX\GnollHackX.Android\assetpacks mkdir ..\xpl\GnollHackX\GnollHackX.Android\assetpacks
 copy c:\wsl-in\assetpack\installtimeassetpack.zip ..\xpl\GnollHackX\GnollHackX.Android\assetpacks
 copy c:\wsl-in\assetpack2\ondemandassetpack.zip ..\xpl\GnollHackX\GnollHackX.Android\assetpacks
+
+if not exist ..\xpl\GnollHackM\Platforms\Android\assetpacks mkdir ..\xpl\GnollHackM\Platforms\Android\assetpacks
+copy c:\wsl-in\assetpack\installtimeassetpack.zip ..\xpl\GnollHackM\Platforms\Android\assetpacks
+copy c:\wsl-in\assetpack2\ondemandassetpack.zip ..\xpl\GnollHackM\Platforms\Android\assetpacks
--- win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in.bat
+++ win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in.bat
@@ -24,14 +24,25 @@ if not exist c:\wsl-in\assetpack2\aapt2output mkdir c:\wsl-in\assetpack2\aapt2ou
 echo Copying banks to c:\wsl-in\assetpack2\Assets\banks
 copy ..\xpl\GnollHackX\GnollHackX.Android.GooglePlay\OnDemand\AboutAssetPack.txt c:\wsl-in\assetpack2\Assets
 
+echo Start copying GnollHackX banks
 if not exist ..\xpl\GnollHackX\GnollHackX.Android\banks mkdir ..\xpl\GnollHackX\GnollHackX.Android\banks
 if not exist ..\xpl\GnollHackX\GnollHackX.iOS\banks mkdir ..\xpl\GnollHackX\GnollHackX.iOS\banks
 del /Q ..\xpl\GnollHackX\GnollHackX.Android\banks\*.*
 del /Q ..\xpl\GnollHackX\GnollHackX.iOS\banks\*.*
-echo Copying banks to Android banks
+echo Copying banks to X Android banks
 copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.Android\banks
-echo Copying banks to iOS banks
+echo Copying banks to X iOS banks
 copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.iOS\banks
+
+echo Start copying GnollHackM banks
+if not exist ..\xpl\GnollHackM\Platforms\Android\banks mkdir ..\xpl\GnollHackM\Platforms\Android\banks
+if not exist ..\xpl\GnollHackM\Platforms\iOS\banks mkdir ..\xpl\GnollHackM\Platforms\iOS\banks
+del /Q ..\xpl\GnollHackM\Platforms\Android\banks\*.*
+del /Q ..\xpl\GnollHackM\Platforms\iOS\banks\*.*
+echo Copying banks to X Android banks
+copy ..\bank\Mobile\*.* ..\xpl\GnollHackM\Platforms\Android\banks
+echo Copying banks to X iOS banks
+copy ..\bank\Mobile\*.* ..\xpl\GnollHackM\Platforms\iOS\banks
 
 echo Copying aapt2 to wsl-in\assetpack
 copy ..\xpl\GnollHackX\GnollHackX.Android.GooglePlay\aapt2 c:\wsl-in\assetpack
