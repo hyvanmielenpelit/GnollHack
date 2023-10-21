2d2e57ff5e728217a86cd280e3fac1a98d60a55b
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2023-10-21 21:20:46 +0300

    Ki-rin can now wear amulets. The Oracle now explains the invocation ritual in the right order.

--- include/monflag.h
+++ include/monflag.h
@@ -369,6 +369,7 @@
 #define M6_SHARK_ANIMATION          0x00800000UL   /* Special animation where the portion above the surface changes */
 #define M6_HUMAN_BREATHE_ANIMATION  0x01000000UL   /* GUI can add a human breathe (2 legs standing) animation */
 #define M6_ANIMAL_BREATHE_ANIMATION 0x02000000UL   /* GUI can add an animal breathe (4 legs standing) animation */
+#define M6_WANTS_TO_WEAR_OBJECTS    0x04000000UL   /* Override animal etc. flags preventing the use of items */
 
 #define M7_NONE                     0x00000000UL
 #define M7_ARCHAEOLOGIST            0x00000001UL   /* monster can use archaeologist items */
--- src/worn.c
+++ src/worn.c
@@ -1387,7 +1387,7 @@ boolean creation;
      * except for the additional restriction on intelligence.  (Players
      * are always intelligent, even if polymorphed).
      */
-    if (!can_operate_objects(mon->data) || is_animal(mon->data))
+    if (!can_operate_objects(mon->data) || (is_animal(mon->data) && !wants_to_wear_objects(mon->data)))
         return;
 
     if (mon->mfrozen)
