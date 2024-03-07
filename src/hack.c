51754667f8e34d5af1033ba449c657fafd6c985d
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2024-03-07 12:45:51 +0200

    Changed trailing mpeaceful = 0 statement to MM_ANGRY flag when trying to make a hostile monster with a makemon call.

--- src/hack.c
+++ src/hack.c
@@ -2875,6 +2875,7 @@ boolean pick;
                 You_ex(ATR_NONE, CLR_MSG_ATTENTION, "surprise %s!",
                     Blind && !sensemon(mtmp) ? something : a_monnam(mtmp));
                 mtmp->mpeaceful = 0;
+                set_mhostility(mtmp);
                 newsym(mtmp->mx, mtmp->my);
             }
             else
