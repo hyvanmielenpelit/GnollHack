ca2ddbdc8167e5f44b30c3a5d21f770fcb7685e6
janne-hmp <janne.gustafsson@hyvanmielenpelit.fi>
2024-08-22 00:57:52 +0300

    Deleted double code in merge.

--- win/win32/xpl/GnollHackX/GnollHackX/MainPage.xaml.cs
+++ win/win32/xpl/GnollHackX/GnollHackX/MainPage.xaml.cs
@@ -1259,12 +1259,6 @@ namespace GnollHackX
             UpperButtonGrid.IsEnabled = true;
         }
 
-        public void StartCarouselViewAndEnableButtons()
-        {
-            carouselView.Play();
-            UpperButtonGrid.IsEnabled = true;
-        }
-
         public void PlayCarouselView()
         {
             carouselView.Play();
