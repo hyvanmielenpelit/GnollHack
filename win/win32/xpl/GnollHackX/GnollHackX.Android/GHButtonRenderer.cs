a9a2a59cf20f556fe492eb82f505de773fa9be6c
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2024-09-01 16:38:18 +0300

    Added custom button renderer for Xamarin Android. This disables (keyboard) focus on all buttons.

---
+++ win/win32/xpl/GnollHackX/GnollHackX.Android/GHButtonRenderer.cs
@@ -0,0 +1,37 @@
+﻿using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using GnollHackX.Droid;
+using GnollHackX.Pages.Game;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.Android;
+
+[assembly: ExportRenderer(typeof(Xamarin.Forms.Button), typeof(GHButtonRenderer))]
+namespace GnollHackX.Droid
+{
+    public class GHButtonRenderer : ButtonRenderer
+    {
+        public GHButtonRenderer(Context context) : base(context)
+        {
+        }
+        protected override void OnElementChanged(ElementChangedEventArgs<Xamarin.Forms.Button> e)
+        {
+            base.OnElementChanged(e);
+            if (e.NewElement != null)
+            {
+                Android.Widget.Button b = Control as Android.Widget.Button;
+                if (b != null)
+                {
+                    b.Focusable = false;
+                }
+            }
+        }
+    }
+}
