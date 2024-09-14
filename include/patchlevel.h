29b6a47c43891b1089a2a79d1c52a5363b0dbbaf
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2024-09-14 16:31:41 +0300

    Changed edit level to 19, Android and iOS version numbers to 1.87 and build numbers to 143, Maui application display number to 2.13, application number to 213, and Windows package minor version to 13.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -19,7 +19,7 @@
  * EDITLEVEL is also used to indicate the pre-alpha / alpha / beta version if PRE_RELEASE compiler preprocessor option is on.
  * Change EDITLEVEL when the changes to the game are save-file-breaking.
  */
-#define EDITLEVEL 18
+#define EDITLEVEL 19
 
 /*
  * HOTFIXLEVEL indicates the hot fix number. 
