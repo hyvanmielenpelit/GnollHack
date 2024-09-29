977bd5ec4044ea660a4ce7a9ef36854f8139df00
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2024-09-29 16:01:41 +0300

    Added a check that WIN_MESSAGE is not WIN_ERR

--- win/tty/wintty.c
+++ win/tty/wintty.c
@@ -1643,7 +1643,7 @@ boolean free_data;
     int i;
 
     if (cw->data) {
-        if (cw == wins[WIN_MESSAGE] && cw->rows > cw->maxrow)
+        if (WIN_MESSAGE != WIN_ERR && cw == wins[WIN_MESSAGE] && cw->rows > cw->maxrow)
             cw->maxrow = cw->rows; /* topl data */
         for (i = 0; i < cw->maxrow; i++)
         {
