aee426357dc147a82508bdd6adfecd4d54b697dd
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2025-04-09 23:12:46 +0300

    Changed edit level to 31, Maui version to 2.23, and Windows package build to 60.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -19,7 +19,7 @@
  * EDITLEVEL is also used to indicate the pre-alpha / alpha / beta version if PRE_RELEASE compiler preprocessor option is on.
  * Change EDITLEVEL when the changes to the game are save-file-breaking.
  */
-#define EDITLEVEL 30
+#define EDITLEVEL 31
 
 /*
  * HOTFIXLEVEL indicates the hot fix number. 
