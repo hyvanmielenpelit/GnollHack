eff4ed78d287a5f0bdea30b42cee938ec83ed134
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2025-04-13 21:23:09 +0300

    Fixes to update-wsl batch files.

--- win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in-debug.bat
+++ win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in-debug.bat
@@ -1,6 +1,13 @@
-echo Start executing update-wsl-in-debug.bat
+echo Executing update-wsl-in-debug.bat
 
-echo Start copying GnollHackX banks
+echo Preparing GnollHack tilesets
+copy ..\tileset\gnollhack_64x96_transparent_32bits.png ..\tileset\gnollhack_64x96_transparent_32bits.ghpng
+copy ..\tileset\gnollhack_64x96_transparent_32bits-2.png ..\tileset\gnollhack_64x96_transparent_32bits-2.ghpng
+
+echo Copying tileset to c:\wsl-in\assetpack\Assets\tileset
+copy ..\tileset\*.ghpng c:\wsl-in\assetpack\Assets\tileset
+
+echo Copying GnollHackX banks
 if not exist ..\xpl\GnollHackX\GnollHackX.Android\banks mkdir ..\xpl\GnollHackX\GnollHackX.Android\banks
 if not exist ..\xpl\GnollHackX\GnollHackX.iOS\banks mkdir ..\xpl\GnollHackX\GnollHackX.iOS\banks
 del /Q ..\xpl\GnollHackX\GnollHackX.Android\banks\*.*
@@ -10,7 +17,17 @@ copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.Android\banks
 echo Copying banks to X iOS banks
 copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.iOS\banks
 
-echo Start copying GnollHackM banks
+echo Copying GnollHackX tileset
+if not exist ..\xpl\GnollHackX\GnollHackX.Android\tileset mkdir ..\xpl\GnollHackX\GnollHackX.Android\tileset
+if not exist ..\xpl\GnollHackX\GnollHackX.iOS\tileset mkdir ..\xpl\GnollHackX\GnollHackX.iOS\tileset
+del /Q ..\xpl\GnollHackX\GnollHackX.Android\tileset\*.*
+del /Q ..\xpl\GnollHackX\GnollHackX.iOS\tileset\*.*
+echo Copying tileset to X Android tileset
+copy ..\tileset\*.ghpng ..\xpl\GnollHackX\GnollHackX.Android\tileset
+echo Copying tileset to X iOS tileset
+copy ..\tileset\*.ghpng ..\xpl\GnollHackX\GnollHackX.iOS\tileset
+
+echo Copying GnollHackM banks
 if not exist ..\xpl\GnollHackM\Platforms\Android\banks mkdir ..\xpl\GnollHackM\Platforms\Android\banks
 if not exist ..\xpl\GnollHackM\Platforms\iOS\banks mkdir ..\xpl\GnollHackM\Platforms\iOS\banks
 if not exist ..\xpl\GnollHackM\Platforms\Windows\banks mkdir ..\xpl\GnollHackM\Platforms\Windows\banks
@@ -23,3 +40,19 @@ echo Copying banks to M iOS banks
 copy ..\bank\Mobile\*.* ..\xpl\GnollHackM\Platforms\iOS\banks
 echo Copying banks to M Windows banks
 copy ..\bank\Desktop\*.* ..\xpl\GnollHackM\Platforms\Windows\banks
+
+echo Copying GnollHackM tileset
+if not exist ..\xpl\GnollHackM\Platforms\Android\tileset mkdir ..\xpl\GnollHackM\Platforms\Android\tileset
+if not exist ..\xpl\GnollHackM\Platforms\iOS\tileset mkdir ..\xpl\GnollHackM\Platforms\iOS\tileset
+if not exist ..\xpl\GnollHackM\Platforms\Windows\tileset mkdir ..\xpl\GnollHackM\Platforms\Windows\tileset
+del /Q ..\xpl\GnollHackM\Platforms\Android\tileset\*.*
+del /Q ..\xpl\GnollHackM\Platforms\iOS\tileset\*.*
+del /Q ..\xpl\GnollHackM\Platforms\Windows\tileset\*.*
+echo Copying tileset to M Android tileset
+copy ..\tileset\*.ghpng ..\xpl\GnollHackM\Platforms\Android\tileset
+echo Copying tileset to M iOS tileset
+copy ..\tileset\*.ghpng ..\xpl\GnollHackM\Platforms\iOS\tileset
+echo Copying tileset to M Windows tileset
+copy ..\tileset\*.ghpng ..\xpl\GnollHackM\Platforms\Windows\tileset
+
+del /Q ..\tileset\*.ghpng
--- win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in-release.bat
+++ win/win32/xpl/GnollHackX/GnollHackX.Android.GooglePlay/update-wsl-in-release.bat
@@ -1,4 +1,4 @@
-echo Start executing update-wsl-in-debug.bat
+echo Executing update-wsl-in-release.bat
 
 if not exist c:\wsl-in mkdir c:\wsl-in
 
@@ -19,7 +19,8 @@ copy ..\bank\Mobile\Auxiliary.bank c:\wsl-in\assetpack\Assets\banks
 copy ..\bank\Mobile\Intro.bank c:\wsl-in\assetpack\Assets\banks
 
 echo Preparing GnollHack tilesets
-copy ..\tileset\*.png ..\tileset\*.ghpng
+copy ..\tileset\gnollhack_64x96_transparent_32bits.png ..\tileset\gnollhack_64x96_transparent_32bits.ghpng
+copy ..\tileset\gnollhack_64x96_transparent_32bits-2.png ..\tileset\gnollhack_64x96_transparent_32bits-2.ghpng
 
 echo Copying tileset to c:\wsl-in\assetpack\Assets\tileset
 copy ..\tileset\*.ghpng c:\wsl-in\assetpack\Assets\tileset
@@ -58,7 +59,7 @@ rem if not exist c:\wsl-in\assetpack-maui2\aapt2output mkdir c:\wsl-in\assetpack
 rem echo Copying text file to c:\wsl-in\assetpack-maui2\Assets\banks
 rem copy ..\xpl\GnollHackX\GnollHackX.Android.GooglePlay\OnDemand-Maui\AboutAssetPack.txt c:\wsl-in\assetpack-maui2\Assets
 
-echo Start copying GnollHackX banks
+echo Copying GnollHackX banks
 if not exist ..\xpl\GnollHackX\GnollHackX.Android\banks mkdir ..\xpl\GnollHackX\GnollHackX.Android\banks
 if not exist ..\xpl\GnollHackX\GnollHackX.iOS\banks mkdir ..\xpl\GnollHackX\GnollHackX.iOS\banks
 del /Q ..\xpl\GnollHackX\GnollHackX.Android\banks\*.*
@@ -68,7 +69,7 @@ copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.Android\banks
 echo Copying banks to X iOS banks
 copy ..\bank\Mobile\*.* ..\xpl\GnollHackX\GnollHackX.iOS\banks
 
-echo Start copying GnollHackX tileset
+echo Copying GnollHackX tileset
 if not exist ..\xpl\GnollHackX\GnollHackX.Android\tileset mkdir ..\xpl\GnollHackX\GnollHackX.Android\tileset
 if not exist ..\xpl\GnollHackX\GnollHackX.iOS\tileset mkdir ..\xpl\GnollHackX\GnollHackX.iOS\tileset
 del /Q ..\xpl\GnollHackX\GnollHackX.Android\tileset\*.*
@@ -78,7 +79,7 @@ copy ..\tileset\*.ghpng ..\xpl\GnollHackX\GnollHackX.Android\tileset
 echo Copying tileset to X iOS tileset
 copy ..\tileset\*.ghpng ..\xpl\GnollHackX\GnollHackX.iOS\tileset
 
-echo Start copying GnollHackM banks
+echo Copying GnollHackM banks
 if not exist ..\xpl\GnollHackM\Platforms\Android\banks mkdir ..\xpl\GnollHackM\Platforms\Android\banks
 if not exist ..\xpl\GnollHackM\Platforms\iOS\banks mkdir ..\xpl\GnollHackM\Platforms\iOS\banks
 if not exist ..\xpl\GnollHackM\Platforms\Windows\banks mkdir ..\xpl\GnollHackM\Platforms\Windows\banks
@@ -92,7 +93,7 @@ copy ..\bank\Mobile\*.* ..\xpl\GnollHackM\Platforms\iOS\banks
 echo Copying banks to M Windows banks
 copy ..\bank\Desktop\*.* ..\xpl\GnollHackM\Platforms\Windows\banks
 
-echo Start copying GnollHackM tileset
+echo Copying GnollHackM tileset
 if not exist ..\xpl\GnollHackM\Platforms\Android\tileset mkdir ..\xpl\GnollHackM\Platforms\Android\tileset
 if not exist ..\xpl\GnollHackM\Platforms\iOS\tileset mkdir ..\xpl\GnollHackM\Platforms\iOS\tileset
 if not exist ..\xpl\GnollHackM\Platforms\Windows\tileset mkdir ..\xpl\GnollHackM\Platforms\Windows\tileset
