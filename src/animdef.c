abc39fca0683c6604d160f83443cb423ea44ec14
Wiiksu <henri.sundstrom88@gmail.com>
2025-11-19 14:11:27 +0200

    Adds attack animation for Baalzebub.

--- src/animdef.c
+++ src/animdef.c
@@ -3828,6 +3828,16 @@ NEARDATA const struct animation_definition animations[MAX_ANIMATIONS] =
       DEMOGORGON_SPECIAL_ATTACK_ENLARGEMENT,
       { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
     },
+    { "baalzebub-attack-animation", ANIMATION_TYPE_NORMAL, 12, 12,
+      1,
+      1,
+      ANIMATION_PLAY_TYPE_PLAYED_SEPARATELY, ANIMATION_MAIN_TILE_IGNORE,
+      AUTODRAW_NONE,
+      { 0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31 },
+      6, 8,
+      BAALZEBUB_ATTACK_ENLARGEMENT,
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 }
+    },
 };
 
 NEARDATA const struct enlargement_definition enlargements[MAX_ENLARGEMENTS] =
@@ -8366,6 +8376,14 @@ NEARDATA const struct enlargement_definition enlargements[MAX_ENLARGEMENTS] =
         { 0, 0, 0, 0, 0 }
       },
       { "baalzebub-statue-enlargement", 0, 0,
+        5,
+        3, 2, 0,
+        { 0, 1, 2, 3, 4 },
+        { 0, 0, 0, 0, 0 },
+        { 0, 0, 0, 0, 0 }
+      },
+      { "baalzebub-attack-enlargement",
+        12, 12,
         5,
         3, 2, 0,
         { 0, 1, 2, 3, 4 },
