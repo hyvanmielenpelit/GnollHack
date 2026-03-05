273ab6d7e238c28d8b7b88a554fee682a6bdc8ae
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2026-03-05 20:28:47 +0200

    Changed edit level to 112, MAUI version to 2.112, MAUI build to 350, and Windows package to 2.112.170.

--- include/patchlevel.h
+++ include/patchlevel.h
@@ -19,7 +19,7 @@
  * EDITLEVEL is also used to indicate the pre-alpha / alpha / beta version if PRE_RELEASE compiler preprocessor option is on.
  * Change EDITLEVEL when the changes to the game are save-file-breaking.
  */
-#define EDITLEVEL 111
+#define EDITLEVEL 112
 
 /*
  * HOTFIXLEVEL indicates the hot fix number. 
