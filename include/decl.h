b88bc1c4540eb60d66cb6893152bbea156b375c2
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2026-04-04 12:10:59 +0300

    Added a few more debug bufs.

--- include/decl.h
+++ include/decl.h
@@ -696,8 +696,8 @@ E const char* const hofe_titles[3];
 
 E uint64_t file_end_marker;
 
-#define NUM_DEBUGBUFS 10
-#define TOTALNUM_DEBUGBUFS 20
+#define NUM_DEBUGBUFS 15
+#define TOTALNUM_DEBUGBUFS 30
 #define DEBUGBUFSIZ (BUFSZ * 2)
 E char debug_buf_array[TOTALNUM_DEBUGBUFS][DEBUGBUFSIZ];
 E char* debug_bufs[TOTALNUM_DEBUGBUFS];
