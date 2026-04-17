4e9be4734f0bb63bd9854ad497a63adc9f7b1687
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2026-04-17 22:45:34 +0300

    Excluded containers from quick engrave items.

--- src/invent.c
+++ src/invent.c
@@ -3349,7 +3349,7 @@ struct obj* otmp_only;
                 || (!strcmp(word, "unmark as favorite") /* exclude if not a favorite */
                     && !(otmp->speflags & SPEFLAGS_FAVORITE))
                 || (!strcmp(word, "set as quick engrave item") /* exclude if already a quick engrave item */
-                    && otmp->o_id == context.quick_engrave_obj_oid)
+                    && (otmp->o_id == context.quick_engrave_obj_oid || Is_container(otmp)))
                 || (!strcmp(word, "unset as quick engrave item") /* exclude if not a quick engrave item */
                     && otmp->o_id != context.quick_engrave_obj_oid)
                 || (!strcmp(word, "set as quick pick-axe") /* exclude if already a quick pick-axe */
