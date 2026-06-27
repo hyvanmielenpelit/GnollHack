614a0702a9fc897b9aaf9cbf38d9c1b6ba8220ee
janne-hmp <53081197+janne-hmp@users.noreply.github.com>
2026-06-27 09:11:22 +0300

    Deleted conversion files.

--- test.c
+++
@@ -1,9 +0,0 @@
-#include <stdio.h>
-#include <stdint.h>
-#define OBJFLAG 0x00000200UL
-int main() { 
-    uint64_t flags = 0xFFFFFFFFFFFFFFFFULL; 
-    flags &= ~OBJFLAG; 
-    printf("%llx\n", (unsigned long long)flags); 
-    return 0; 
-}
